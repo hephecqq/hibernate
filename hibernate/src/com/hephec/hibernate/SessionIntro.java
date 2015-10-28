b634ba5c6bb0d899eb93a9103e090eed6ad0edf6
Administrator <Administrator@QH-20150815QEPL>
2015-10-28 22:15:48 +0800

    comm

--- hibernate/src/com/hephec/hibernate/SessionIntro.java
+++ hibernate/src/com/hephec/hibernate/SessionIntro.java
@@ -1,14 +1,14 @@
 package com.hephec.hibernate;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 public class SessionIntro {
 	/**
